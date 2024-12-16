b1e858561cde1f9f5f594dcf42a82aa3d883b2a0
G-S-RAMYA <ramyags1802@gmail.com>
2024-12-16 12:05:14 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -3,14 +3,13 @@ class factorial
 {
 public static void main(String args[])
 {
-Scanner fac=new Scanner(System.in);
-System.out.println("Enter a number:");
-int x=fac.nextInt();
-
-int fact=1;
+int x=5;
+  int fact=1;
 for(int i=1;i<=x;i++)
 fact=fact*i;
 System.out.println("Factorial is " +fact);
 }
 }
+
+
 
